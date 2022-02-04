3db0b0bf054eb6bcb0b9e95f177226511d081270
Guilherme <45200933+cguiama@users.noreply.github.com>
2022-02-04 18:30:46 -0300

    First Commit

---
+++ src/ContaCorrente.java
@@ -0,0 +1,42 @@
+public class ContaCorrente {
+
+    private int agencia;
+    private int numero;
+    private int digito;
+    private int saldo;
+
+    //construtores
+
+
+    //metodos
+
+
+    public void sacar() {
+
+    }
+
+    public void depositar() {
+
+    }
+
+    public void transferir() {
+
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public int getDigito() {
+        return digito;
+    }
+
+    public int getSaldo() {
+        return saldo;
+    }
+
+}
---
+++ src/ContaPoupanca.java
@@ -0,0 +1,16 @@
+public class ContaPoupanca {
+    private int agencia;
+    private int numero;
+    private int digito;
+    private int saldo;
+
+    public void sacar(){
+
+    }
+    public void depositar(){
+
+    }
+    public void transferir(){
+
+    }
+}
